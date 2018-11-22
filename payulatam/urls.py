9b85ccb5d2d4aa24143ffe512c30518eb31f288a
Gustavo Saavedra <tavito.286@gmail.com>
2018-11-22 11:58:40 -0500

    Django url/path support for 1.X/2.0+

--- payulatam/urls.py
+++ payulatam/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+try:
+    # Try to import path from django 2.0+
+    from django.urls import path
+    url = 'notification/'
+except:
+    # If it breaks then we import url from django 1.X
+    from django.conf.urls import url as path
+    url = '^notification/$'
 
 from payulatam.views import PaymentNotificationView
 
 urlpatterns = [
-    path('notification/', PaymentNotificationView.as_view(), name='notification'),
+    path(url, PaymentNotificationView.as_view(), name='notification'),
 ]
