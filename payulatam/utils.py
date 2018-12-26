827762e623564ff8510b15f36ed5b30fab292278
Gustavo Saavedra <tavito.286@gmail.com>
2018-12-26 12:04:35 -0500

    Agrega soporte para signature de creación

--- payulatam/utils.py
+++ payulatam/utils.py
@@ -1,6 +1,13 @@
 import hashlib
 
 
-def get_signature(api_key, merchant_id, reference_code, tx_value, currency, state_pol):
-    signature = '{}~{}~{}~{}~{}~{}'.format(api_key, merchant_id, reference_code, tx_value, currency, state_pol)
+def get_signature(api_key, merchant_id, reference_code, tx_value, currency, state_pol=None):
+    """
+        Genera el signature requerido por PayU al crear y verificar una transacción.
+        El state_pol solo es necesario para confirmar la transacción.
+    """
+    if state_pol:
+        signature = '{}~{}~{}~{}~{}~{}'.format(api_key, merchant_id, reference_code, tx_value, currency, state_pol)
+    else:
+        signature = '{}~{}~{}~{}~{}'.format(api_key, merchant_id, reference_code, tx_value, currency)
     return hashlib.md5(signature.encode('utf')).hexdigest()
